c36bebf39b317b5cde05dacbbd52c71e2009020f
Goderich <yuhsien77@gmail.com>
2020-10-20 17:25:35 +0800

    Adjust parameters on model training script

--- w2v-train.py
+++ w2v-train.py
@@ -2,5 +2,5 @@ from gensim.models import word2vec
 import sys
 
 sentences = word2vec.LineSentence(sys.argv[1])
-model = word2vec.Word2Vec(sentences, size=1, sg=1)#, min_count=1)
+model = word2vec.Word2Vec(sentences, size=300, min_count=1, sg=1, window=3)
 model.save(sys.argv[2])
